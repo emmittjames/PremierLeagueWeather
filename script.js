f0355b5be88e4fa07b389a029fc29e4692c88a26
emmittjames <emmittjames1@gmail.com>
2022-11-09 18:26:44 -0500

    Weather API

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+//const url = "https://api.openweathermap.org/data/2.5/weather?q={London}&appid={55adcacc18884e6c11d90a5bb7f97e31}&units=imperial";
+
 let weather = {
-    "apiKey": "55adcacc18884e6c11d90a5bb7f97e31"
-    
+    "apiKey": "55adcacc18884e6c11d90a5bb7f97e31",
+    fetchWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            +city
+            +"&appid=" + this.apiKey
+            +"&units=" + "imperial"
+        )
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+        //displayWeather: function(data){
+
+        //}
+    }
 }
+
+weather.fetchWeather("Charlottesville")
