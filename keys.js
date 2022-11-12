12bf3c8531b43ee67f5194cbc2f76d0bc2d425fc
emmittjames <emmittjames1@gmail.com>
2022-11-11 19:39:49 -0500

    Removing more junk

--- keys.js
+++
@@ -1,6 +0,0 @@
-const weatherKey = "55adcacc18884e6c11d90a5bb7f97e31"
-const footyKey = "14499cf8bcmsh49dd9f051925dbfp1311fcjsna203bcd62a2d"
-
-export function getFootyKey(){
-    return '14499cf8bcmsh49dd9f051925dbfp1311fcjsna203bcd62a2d'
-}
