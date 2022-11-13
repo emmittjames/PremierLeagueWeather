1200aef1d77f2e92328fade3632f507a2ec6822f
emmittjames <emmittjames1@gmail.com>
2022-11-12 19:41:36 -0500

    API now only calls once a day

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-async function fetchPrem(year){
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': config.footyKey,
-            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-        }
-    };
-    const response = await(fetch("https://api-football-v1.p.rapidapi.com/v3/standings?season=" + year + "&league=39", options))
-    const data = await response.json()
-    return getTeamNames(data)
+function getDataFromLocalStorage() {
+    if (typeof window !== 'undefined') {
+        const data = JSON.parse(localStorage.getItem("prem"))
+        return data
+    }
+}  
+
+function isDataOutdated(data){
+    const date = new Date(data["date"])
+    const today = new Date();
+    if(!date || date.getDay()<today.getDay()){
+        console.log("fetching new data")
+        return true
+    }
+    return false
 }
 
-async function getTeamNames(data){
+async function fetchPrem(year){
+    let data = getDataFromLocalStorage();
+    if(!data || isDataOutdated(data)) {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': config.footyKey,
+                'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+            }
+        }
+        const response = await(fetch("https://api-football-v1.p.rapidapi.com/v3/standings?season=" + year + "&league=39", options))
+        const data = await response.json()
+        const teamNames = getTeamNames(data)
+        localStorage.setItem("prem", 
+            JSON.stringify({ "teamNames": teamNames, "date": new Date()})
+        )
+        return getDataFromLocalStorage()["teamNames"];
+    }
+    return data["teamNames"]
+}
+
+function getTeamNames(data){
     let teamNames = []
     const league = data["response"][0]["league"]
     const teams = league["standings"][0]
@@ -52,10 +77,11 @@ async function calculateTeam(city){
     const name = weatherData[0]
     const temp = Math.floor(weatherData[1])
     const description = weatherData[2]
-    const index = calculateIndex(temp)
+    const index = calculateteamIndex(temp)
     const message = getMessage(temp)
 
     const selectedTeam = teamNames[index]
+    console.log(teamNames)
     console.log(selectedTeam)
     console.log(name)
     console.log(temp)
@@ -76,8 +102,8 @@ function changePicture(team){
     document.body.style.backgroundImage = "url(Pictures/" + team + ".jpeg)"
 }
 
-function calculateIndex(temperature){
-    const random = Math.floor(Math.random()*3)
+function calculateteamIndex(temperature){
+    let random = Math.floor(Math.random()*3)
     const posNeg = Math.random();
     if(posNeg>.5){
         random*=-1
